b1a6ff1c91e18167c687db8f8fdc5f5b14e71d17
Avinash Rath <83135691+Newt-b1t@users.noreply.github.com>
2025-06-03 00:54:55 +0530

    Add files via upload

---
+++ 22052370-Sprint2Day3/src/main/java/com/nisum/di/TextEditor.java
@@ -0,0 +1,16 @@
+package com.nisum.di;
+
+//for constructor injection
+public class TextEditor {
+    private SpellChecker spellChecker;
+
+    public TextEditor(SpellChecker spellChecker) {
+        System.out.println("Constructor Injection in TextEditor");
+        this.spellChecker = spellChecker;
+    }
+
+    public String spellCheck() {
+        return spellChecker.checkSpelling();
+    }
+
+}
